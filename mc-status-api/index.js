181ca7e4459c01105992d265f0cc84a6f71d470a
Nonthapat Vinijpol <6710110590@psu.ac.th>
2026-02-09 20:02:40 +0700

    starting fresh day

--- mc-status-api/index.js
+++ mc-status-api/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 /* ================== CONFIG ================== */
-const SERVER_IP = "34.15.140.2";
+const SERVER_IP = "34.15.129.81";
 const SERVER_PORT = 25565;
 const JWT_SECRET = "secret"; 
 
